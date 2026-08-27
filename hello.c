96625ef42b9060b8a3084a679c99f55506a29efa
Jose Lopez <josellzzthe@gmail.com>
2026-08-27 12:53:29 -0500

    Added my name

--- hello.c
+++ hello.c
@@ -13,5 +13,7 @@ int main(int argc, char **argv) {
 
   printf("Hello World!\n");
   printf("I am a computer engineering major\n");
+  printf("My name is Jose Lopez and computer Engineering\n");
+  
   return 0;
 }
